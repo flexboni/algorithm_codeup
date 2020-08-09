78320cd6f93f991e380037ceddcc3b515754eec2
flexboni <kgb8594@gmail.com>
2020-08-09 22:38:48 +0900

    1024 : [기초-입출력] 단어 1개 입력받아 나누어 출력하기(설명)

---
+++ 1024/myCode.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    char word[30];
+    scanf("%s", &word);
+    for (int i = 0; word[i] != '\0'; i++)
+    {
+        printf("\'%c\'\n", word[i]);
+    }
+    return 0;
+}
