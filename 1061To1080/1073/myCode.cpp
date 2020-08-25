5558900a4bc4effa16dccb30edd794e5f65643f7
flexboni <kgb8594@gmail.com>
2020-08-25 10:18:32 +0900

    1073 : [기초-반복실행구조] 0 입력될 때까지 무한 출력하기2(설명)

---
+++ 1061To1080/1073/myCode.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    do
+    {
+        scanf("%d", &a);
+
+        if (a != 0)
+            printf("%d\n", a);
+    } while (a != 0);
+
+    return 0;
+}
