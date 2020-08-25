7abce2f68a01302edcb84231deadc938131d4214
flexboni <kgb8594@gmail.com>
2020-08-25 09:51:58 +0900

    1072 : [기초-반복실행구조] 정수 입력받아 계속 출력하기(설명)

---
+++ 1061To1080/1072/myCode.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d", &a);
+reload:
+    scanf("%d", &b);
+
+    if (a-- > 0)
+    {
+        printf("%d\n", b);
+        goto reload;
+    }
+    return 0;
+}
