2e1a24f3fa952f2c7869c0816282c3916d1ed0fa
flexboni <kgb8594@gmail.com>
2020-08-28 10:59:31 +0900

    1085 : [기초-종합] 소리 파일 저장용량 계산하기(설명)

---
+++ 1081To1100/1085/myCode.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    float h, b, c, s;
+
+    scanf("%f %f %f %f", &h, &b, &c, &s);
+
+    printf("%.1f MB", (h * b * c * s) / (1024 * 1024 * 8));
+    return 0;
+}
