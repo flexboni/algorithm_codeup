15850bf02fee059d7f5a0bc7d93e26ae739ff040
flexboni <kgb8594@gmail.com>
2020-09-01 11:57:08 +0900

    1096 : [기초-2차원배열] 바둑판에 흰 돌 놓기(설명)

---
+++ 1081To1100/1096/myCode.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, x, y, arr[20][20] = {0,0};
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d %d", &x, &y);
+        arr[x][y] = 1;
+    }
+
+    for (int i = 1; i <= 19; i++)
+    {
+        for (int j = 1; j <= 19; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
